39305043e3a94ca048fa60fa805d4adf34b9cc38
Subhi-c <csubhiksha23@gmail.com>
2024-06-10 06:42:15 +0530

    Certificates view work done

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -1,8 +1,20 @@
 import EditContent from "./EditContent";
+import { useState, useEffect } from "react";
 function Certificates({ ConData, setConData }) {
+  const [Certificate, setCertificate] = useState([]);
+  useEffect(() => {
+    setCertificate(ConData.certifications);
+    console.log(Certificate);
+  });
   return (
     <>
-      <h1>Certificates</h1>{" "}
+      {Certificate.map((Certi, index) => (
+        <div key={index}>
+          <h1>{Certi.name}</h1>
+          <p>{Certi.issuing_organization}</p>
+          <p>{Certi.issue_date}</p>
+        </div>
+      ))}
     </>
   );
 }
--- src/components/EditCertificates.jsx
+++ src/components/EditCertificates.jsx
@@ -1,4 +1,52 @@
-function EditCertificates({ Data, setData }) {
-  return <></>;
+import { useState, useEffect } from "react";
+function EditCertificates({
+  section,
+  ConData,
+  setConData,
+  editsection,
+  seteditsection,
+}) {
+  const [Certificate, setCertificate] = useState([]);
+  useEffect(() => {
+    setCertificate(ConData.certifications);
+    console.log(Certificate, "from edit section");
+  });
+  return (
+    <>
+      <div>
+        {Certificate.map((certi, index) => {
+          <div key={index}>
+            <div>
+              <label htmlFor="name">Name :</label>
+              <input
+                type="text"
+                name="name"
+                id={`name${index}`}
+                value={certi.name}
+              />
+            </div>
+            <div>
+              <label htmlFor="issuing_organization">Organization :</label>
+              <input
+                type="text"
+                name="issuing_organization"
+                id={`issuing_organization${index}`}
+                value={certi.issuing_organization}
+              />
+            </div>
+            <div>
+              <label htmlFor="issue_date">Date :</label>
+              <input
+                type="month"
+                name="issue_date"
+                id={`issue_date${index}`}
+                value={certi.issue_date}
+              />
+            </div>
+          </div>;
+        })}
+      </div>
+    </>
+  );
 }
 export default EditCertificates;
